cdcf4a5fa03d209455232ed8397691022ae03e0e
sam <samliao1@auforce.com>
2024-09-02 11:14:13 +0800

    ✨ feat: 新增 .gitignore

---
+++ ConsoleApp1/Program.fs
@@ -0,0 +1,9 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+let square x = x * x
+let numbers = [1 .. 10]
+let squares = List.map square numbers
+printfn "N^2 = %A" squares
+
+
+printfn "Press any key to continue..."
+System.Console.ReadKey() |> ignore 
